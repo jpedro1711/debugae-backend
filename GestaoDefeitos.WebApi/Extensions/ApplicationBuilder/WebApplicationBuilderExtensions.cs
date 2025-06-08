2da73cd8526b4dfe769febb1207a08f50376f9a4
jpedro1711 <joaopedroigeski@gmail.com>
2025-06-08 18:44:35 -0300

    feat: ExtensionMethods

---
+++ GestaoDefeitos.WebApi/Extensions/ApplicationBuilder/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,80 @@
+﻿using GestaoDefeitos.Domain.Entities.Contributor;
+using GestaoDefeitos.Infrastructure.Database;
+using GestaoDefeitos.WebApi.Extensions.Endpoints;
+using GestaoDefeitos.WebApi.Extensions.Migrations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoDefeitos.WebApi.Extensions.ApplicationBuilder
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddBaseServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthorization();
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder ConfigureIdentity(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+            })
+            .AddCookie(IdentityConstants.ApplicationScheme, options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromHours(1);
+                options.SlidingExpiration = true;
+            })
+            .AddBearerToken(IdentityConstants.BearerScheme);
+
+            builder.Services.AddIdentityCore<Contributor>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddApiEndpoints();
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder ConfigureDatabase(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            return builder;
+        }
+
+        public static WebApplication ConfigureWebApplication(this WebApplication webApplication)
+        {
+            if (webApplication.Environment.IsDevelopment())
+            {
+                webApplication.UseSwagger();
+                webApplication.UseSwaggerUI();
+            }
+
+            webApplication.ApplyMigrations();
+
+            webApplication.UseHttpsRedirection();
+
+            webApplication.UseAuthorization();
+
+            webApplication.MapControllers();
+
+            webApplication.MapIdentityApi<Contributor>();
+
+            return webApplication;
+        }
+
+        public static WebApplication MapEndpoints(this WebApplication webApplication)
+        {
+            webApplication.MapContributorEndpoints();
+
+            return webApplication;
+        }
+    }
+}
---
+++ GestaoDefeitos.WebApi/Extensions/Endpoints/ContributorEndpoints.cs
@@ -0,0 +1,45 @@
+﻿using GestaoDefeitos.Application.Requests;
+using GestaoDefeitos.Domain.Entities.Contributor;
+using GestaoDefeitos.Infrastructure.Database;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace GestaoDefeitos.WebApi.Extensions.Endpoints
+{
+    public static class ContributorEndpoints
+    {
+        public static IEndpointRouteBuilder MapContributorEndpoints(this IEndpointRouteBuilder endpoints)
+        {
+            var group = endpoints.MapGroup("/contributors")
+                .WithTags("Contributors");
+
+            group.MapGet("/me", async (ClaimsPrincipal claims, AppDbContext context) =>
+            {
+                string userId = claims.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+                return await context.Users.FindAsync(userId);
+            })
+            .RequireAuthorization();
+
+            group.MapPost("/register", async (
+                UserManager<Contributor> userManager,
+                RegisterContributorRequest request) =>
+            {
+                var user = new Contributor
+                {
+                    Email = request.Email,
+                    FullName = request.FullName,
+                    UserName = request.Email,
+                };
+
+                var result = await userManager.CreateAsync(user, request.Password);
+
+                if (!result.Succeeded)
+                    return Results.BadRequest(result.Errors);
+
+                return Results.Created($"/users/{user.Id}", new { user.Id, user.UserName, user.Email, user.FullName });
+            });
+
+            return endpoints;
+        }
+    }
+}
