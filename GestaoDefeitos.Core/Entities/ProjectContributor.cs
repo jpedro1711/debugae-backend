ee12a79bfc0afebdcbced9e2b5502da1a3330348
jpedro1711 <joaopedroigeski@gmail.com>
2025-06-16 20:58:23 -0300

    feat: fix project contributor

---
+++ GestaoDefeitos.Core/Entities/ProjectContributor.cs
@@ -0,0 +1,15 @@
+﻿using GestaoDefeitos.Domain.Enums;
+
+namespace GestaoDefeitos.Domain.Entities.RelationEntities
+{
+    public class ProjectContributor
+    {
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; } = null!;
+        public Guid ContributorId { get; set; }
+        public Contributor Contributor { get; set; } = null!;
+        public ProjectRole Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
